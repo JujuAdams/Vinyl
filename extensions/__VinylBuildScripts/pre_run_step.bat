a4d067fbf9324791110d74373f05a1f38e2a86d6
Juju Adams <julian.adams@email.com>
2023-01-31 20:03:32 +0000

    Removes JSON from datafiles

--- extensions/__VinylBuildScripts/pre_run_step.bat
+++ extensions/__VinylBuildScripts/pre_run_step.bat
@@ -1,3 +1,2 @@
 @echo off
 "%YYprojectDir%\extensions\__VinylBuildScripts\base64.exe" /e "%YYprojectDir%\notes\__VinylConfig\__VinylConfig.txt" "%YYprojectDir%\datafiles\vinyl.dat"
-copy /y "%YYprojectDir%\notes\__VinylConfig\__VinylConfig.txt" "%YYprojectDir%\datafiles\vinyl.json"
--- extensions/__VinylBuildScripts/pre_run_step.sh
+++ extensions/__VinylBuildScripts/pre_run_step.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 openssl base64 -in "$YYprojectDir/notes/__VinylConfig/__VinylConfig.txt" -out "$YYprojectDir/datafiles/vinyl.dat"
-cp "$YYprojectDir/notes/__VinylConfig/__VinylConfig.txt" "$YYprojectDir/datafiles/vinyl.json"
 
 exit 0
