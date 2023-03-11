aa984d052c01835dd633dde4c67c90f18641ef8e
Juju Adams <julian.adams@email.com>
2023-03-11 10:35:58 +0000

    Fixes vinyl.dat encoding when compiling on MacOS

--- extensions/__VinylBuildScripts/pre_run_step.sh
+++ extensions/__VinylBuildScripts/pre_run_step.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-openssl base64 -in "$YYprojectDir/notes/__VinylConfig/__VinylConfig.txt" -out "$YYprojectDir/datafiles/vinyl.dat"
+openssl base64 -in "$YYprojectDir/notes/__VinylConfig/__VinylConfig.txt" | tr -d '\n' > "$YYprojectDir/datafiles/vinyl.dat"
 
 exit 0
