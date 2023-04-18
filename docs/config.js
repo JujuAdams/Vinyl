c5c06ffd311d5aa27c29bb395f339ec963aa8c17
Juju Adams <julian.adams@email.com>
2023-04-18 17:05:10 +0100

    Version bump to 5.3

--- docs/config.js
+++ docs/config.js
@@ -22,8 +22,8 @@
 const config = {
 	name: 'Vinyl',
 	description: 'Modular audio system for GameMaker 2023.2',
-	latestVersion: '5.2',
-	otherVersions: ['5.1', '5.0', '4.1', '4.0'],
+	latestVersion: '5.3',
+	otherVersions: ['5.2', '5.1', '5.0', '4.1', '4.0'],
 	favicon: 'assets/favicon.ico',
 	themeColor: '#feab6d',
 };
