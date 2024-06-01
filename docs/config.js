077218546deca5e2580539e0cd8e5698e8f6db66
Juju Adams <julian.adams@email.com>
2024-06-01 17:30:34 +0100

    Update config.js

--- docs/config.js
+++ docs/config.js
@@ -22,8 +22,8 @@
 const config = {
 	name: 'Vinyl',
 	description: 'Modular audio system for GameMaker 2023.8',
-	latestVersion: '5.5',
-	otherVersions: ['6.0', '5.4', '5.3', '5.2', '5.1', '5.0', '4.1', '4.0'],
+	latestVersion: '6.0',
+	otherVersions: ['5.5', '5.4', '5.3', '5.2', '5.1', '5.0', '4.1', '4.0'],
 	favicon: 'assets/favicon.ico',
 	themeColor: '#feab6d',
 };
